5796dc4127e081cfde888e09da7c099b279a3115
heenakhan4 <khanheena4997@gmail.com>
2024-01-29 20:37:56 +0530

    added files for experimental frontend

--- API/urls.py
+++ API/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import inscribing_square_api, circumference_api, multi_circumference_api, convert_coordinates_api
-
+from .api import inscribing_square_api, circumference_api, multi_circumference_api, convert_coordinates_api
+from .views import experimental_frontend_view
 urlpatterns = [
     path('square_api',inscribing_square_api.as_view()),
     path('circumference_api',circumference_api.as_view()),
     path('multi_circumference/',multi_circumference_api.as_view()),
-    # path('conversion',conversion_api.as_view()),
-    path('convert_coordinates/',convert_coordinates_api.as_view())
+    path('convert_coordinates/',convert_coordinates_api.as_view()),
+    path('coordinate-calculator/', experimental_frontend_view, name='coordinate_calculator')
 ]
