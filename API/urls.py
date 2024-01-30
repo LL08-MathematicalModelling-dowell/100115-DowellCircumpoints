6561576c77736a822f782853dcca7e63ec896810
heenakhan4 <khanheena4997@gmail.com>
2024-01-30 20:28:46 +0530

    created views & urls for exp frontend

--- API/urls.py
+++ API/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .api import inscribing_square_api, circumference_api, multi_circumference_api, convert_coordinates_api
-from .views import experimental_frontend_view
+from .views import experimental_frontend_view, inscribing_api_view
 urlpatterns = [
     path('square_api',inscribing_square_api.as_view()),
     path('circumference_api',circumference_api.as_view()),
     path('multi_circumference/',multi_circumference_api.as_view()),
     path('convert_coordinates/',convert_coordinates_api.as_view()),
-    path('coordinate-calculator/', experimental_frontend_view, name='coordinate_calculator')
+    path('coordinate-calculator/', experimental_frontend_view, name='coordinate_calculator'),
+    path('coordinate-calculator/inscribing_api_view', inscribing_api_view, name='coordinates_page')
 ]
--- API/views.py
+++ API/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
+from .api import inscribing_square_api
+from .dowellinscribing import circle_inscribing_api
+import json
 
 def experimental_frontend_view(request):
     return render(request, 'input.html')
 
+def inscribing_api_view(request):
+    if request.method == "GET":
+        dataType = request.GET.get('dataType')
+        shape = request.GET.get('shapeType')
+        canvasLength = int(request.GET.get('length'))
+        canvasWidth = int(request.GET.get('width'))
+        circleRadius = int(request.GET.get('circleRadius',''))
+        # sideLength = int(request.GET.get('squareSideLength',''))
+        gpsDeviceCenters = request.GET.get('gpsDeviceCenters','')
+        
+        if shape == 'circles':
+            output = json.loads(circle_inscribing_api(length = canvasLength, width = canvasWidth, radius = circleRadius))
+            print(output)
+            print(type(output))
+        
+        data = {'shape':shape,
+                'dataType':dataType,
+                'canvasLength':canvasLength,
+                'canvasWidth':canvasWidth,
+                'circleRadius':circleRadius,
+                # 'sideLength':sideLength,
+                'output':output}
+        print(data)
+        
+        return render(request,'inscribe.html',data)
     
