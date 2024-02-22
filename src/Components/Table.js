1699f7957400d75ca9f797077a6b4cdd3f28ee63
Yonas <yonasseyoum24@gmail.com>
2024-02-22 06:03:37 +0300

    add stylings

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -2,27 +2,31 @@ import React from "react";
 
 export default function Table({ data }) {
   return (
-    <table border={1}>
-      <thead>
-        <tr>
-          <th></th>
-          {data[0].map((column, columnIndex) => (
-            <th key={columnIndex}>{column[1].toFixed(1)}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            <th>{row[0][0].toFixed(1)}</th>
-            {row.map((coordinates, columnIndex) => (
-              <td key={`${rowIndex}_${columnIndex}`}>
-                {`[${coordinates[0].toFixed(1)}, ${coordinates[1].toFixed(1)}]`}
-              </td>
+    <div className="table-container">
+      <table border={1} className="table">
+        <thead className="thead">
+          <tr className="trhead">
+            <th></th>
+            {data[0].map((column, columnIndex) => (
+              <th className="th" key={columnIndex}>
+                {column[1]}
+              </th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr key={rowIndex} className="trbody">
+              <th>{row[0][0]}</th>
+              {row.map((coordinates, columnIndex) => (
+                <td key={`${rowIndex}_${columnIndex}`} className="td">
+                  {`[${coordinates[0]}, ${coordinates[1]}]`}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
