21ffb74e79d1cc22e1f0fc2dbd4337b3caec07d8
Yonas <yonasseyoum24@gmail.com>
2024-02-26 12:03:12 +0300

    add chart component

---
+++ src/Pages/components/Chart.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  CartesianGrid,
+  Scatter,
+  ScatterChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+export default function Chart({ data }) {
+  return (
+    <ScatterChart width={1000} height={550}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis type="number" dataKey="x" name="X" />
+      <YAxis type="number" dataKey="y" name="Y" />
+      <Tooltip cursor={{ strokeDasharray: "1 1" }} fill="red" />
+
+      {data.map((circumPoints, index) => (
+        <Scatter key={index} data={circumPoints} fill="#154078" />
+      ))}
+    </ScatterChart>
+  );
+}
