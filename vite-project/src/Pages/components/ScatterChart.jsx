0821d391d053ae7f71298e4abaea9adb12b1acf9
Yonas <yonasseyoum24@gmail.com>
2024-03-08 11:50:26 +0300

    add zoom plugin to the Scatter chart

--- vite-project/src/Pages/components/ScatterChart.jsx
+++ vite-project/src/Pages/components/ScatterChart.jsx
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { Scatter } from "react-chartjs-2";
 import { Chart, registerables } from "chart.js";
-import "chartjs-plugin-zoom"
+import zoomPlugin from "chartjs-plugin-zoom";
 
-Chart.register(...registerables)
+Chart.register(...registerables, zoomPlugin);
 
 export default function ScatterChart({ center, data }) {
-  
-  const flatData = data.flat()
-  const maxX = Math.max(...flatData.map(point => point.x))
-  const maxY = Math.max(...flatData.map(point => point.y))
+  const flatData = data.flat();
+  const maxX = Math.max(...flatData.map((point) => point.x));
+  const maxY = Math.max(...flatData.map((point) => point.y));
 
-  const maxPoint = Math.max(maxX, maxY)
+  const chartRef = useRef(null);
+
+  const maxPoint = Math.max(maxX, maxY);
 
   const getRandomColor = () => {
     const letters = "0123456789ABCDEF";
@@ -23,53 +24,84 @@ export default function ScatterChart({ center, data }) {
     return color;
   };
 
-  const dataSets = []
+  const dataSets = [];
 
-    for(let i = 0; i < center.length; i++){
-      dataSets.push({
-        label: center[i],
-        data: data[i],
-        backgroundColor: getRandomColor(),
-      })
+  for (let i = 0; i < center.length; i++) {
+    dataSets.push({
+      label: center[i],
+      data: data[i],
+      backgroundColor: getRandomColor(),
+    });
+  }
+
+  const handleResetZoom = () => {
+    if (chartRef && chartRef.current) {
+      chartRef.current.resetZoom();
     }
+  };
 
   const options = {
-    title: {display: true, text: "My Chart"},
-    
-    scales: {
-
-      x: {
-        ticks:{
-          color:"black",
+    plugins: {
+      zoom: {
+        zoom: {
+          wheel: {
+            enabled: true,
+          },
+          pinch: {
+            enabled: true,
+          },
+          pan: {
+            enabled: true,
+          },
+          mode: "xy",
         },
-        min: -maxPoint-1,
-        max: maxPoint+1,
+      },
+    },
+
+    scales: {
+      x: {
+        ticks: {
+          color: "black",
+        },
+        min: -maxPoint - 1,
+        max: maxPoint + 1,
         position: "center",
         beginAtZero: true,
-        
-
       },
       y: {
-        ticks:{
-          color:"black",
+        ticks: {
+          color: "black",
         },
-        min: -maxPoint-1,
-        max: maxPoint+1,
+        min: -maxPoint - 1,
+        max: maxPoint + 1,
         position: "center",
         beginAtZero: true,
-      },    
+      },
     },
-    devicePixelRatio:2,
-    maintainAspectRatio: false
+    devicePixelRatio: 2,
+    maintainAspectRatio: false,
   };
 
   const scatterDataSet = {
-    datasets: dataSets
+    datasets: dataSets,
   };
 
   return (
-    <div style={{width:"60%"}}>
-      <Scatter data={scatterDataSet} options={options} width={800} height={500} />
-    </div>
+    <>
+      <div className="scatter-chart-container" style={{ width: "100%" }}>
+        <div>
+          <Scatter
+            ref={chartRef}
+            data={scatterDataSet}
+            options={options}
+            width={500}
+            height={500}
+          />
+        </div>
+        <button className="button reset-button" onClick={handleResetZoom}>
+          Reset Zoom
+        </button>
+      </div>
+    </>
   );
 }
