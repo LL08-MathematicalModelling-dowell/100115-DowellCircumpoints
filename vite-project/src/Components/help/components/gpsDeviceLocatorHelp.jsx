dbb261bb8b54e5de1230b833d4de90133524a879
Yonas <yonasseyoum24@gmail.com>
2024-03-09 10:03:23 +0300

    add components to be rendered by the help component

---
+++ vite-project/src/Components/help/components/gpsDeviceLocatorHelp.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function GpsDeviceLocatorHelp() {
+  return (
+    <div>
+      <div>Centers of the GPS Device</div>
+      <div>Radius</div>
+    </div>
+  );
+}
